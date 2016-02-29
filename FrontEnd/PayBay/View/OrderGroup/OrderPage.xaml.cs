087ebaef9bd05da9f76ce86143346459e69d73c3
darkomega9408 <darkomega9408@yahoo.com>
2016-02-29 17:53:06 +0700

    + Added Progressbar and shadow in Order Page

--- FrontEnd/PayBay/View/OrderGroup/OrderPage.xaml.cs
+++ FrontEnd/PayBay/View/OrderGroup/OrderPage.xaml.cs
@@ -27,5 +27,49 @@ namespace PayBay.View.OrderGroup
         {
             this.InitializeComponent();
         }
+
+        public static T FindVisualChild<T>(DependencyObject parent, string childName) where T : DependencyObject
+        {
+            if (parent == null)
+                return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                T childType = child as T;
+                if (childType == null)
+                {
+                    foundChild = FindVisualChild<T>(child, childName);
+
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    {
+                        foundChild = (T)child;
+                        break;
+                    }
+                }
+                else
+                {
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+
+            return foundChild;
+        }
+
+        public void pivotOrder_SelectionChanged(object sender,SelectionChangedEventArgs e)
+        {
+            //ProgressBar progbar = FindVisualChild<ProgressBar>(pivotstyleorder, "progressBarOrder");
+            //progbar.Value -= 10;
+            
+        }
     }
 }

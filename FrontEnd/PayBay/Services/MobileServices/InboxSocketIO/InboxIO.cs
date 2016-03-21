b0aabe179caf56335db6509564611f2ae895219a
Mon Pham <Mon Pham>
2016-03-21 20:02:40 +0700

    .fix inbox

--- FrontEnd/PayBay/Services/MobileServices/InboxSocketIO/InboxIO.cs
+++ FrontEnd/PayBay/Services/MobileServices/InboxSocketIO/InboxIO.cs
@@ -15,113 +15,25 @@ namespace PayBay.Services.MobileServices.InboxSocketIO
 {
     public class InboxIO
     {
-        private static InboxIO m_Instance = null;
+        //private static InboxIO m_Instance = null;
         private static string HostURL = "http://immense-reef-32079.herokuapp.com/";
 
-        public static InboxIO Instance
-        {
-            get
-            {
-                return m_Instance ?? (m_Instance = new InboxIO(HostURL, 0));
-            }
+        //public static InboxIO Instance
+        //{
+        //    get
+        //    {
+        //        return m_Instance ?? (m_Instance = new InboxIO(HostURL, 0));
+        //    }
 
-            protected set
-            {
-                m_Instance = value;
-            }
-        }
-        private Socket _socket;
-        //private String _customerID;
-        private ObservableCollection<MessageInbox> _messageList;
-        //private String receivedMessage;
-        MessageInbox receivedMessage;
+        //    protected set
+        //    {
+        //        m_Instance = value;
+        //    }
+        //}
 
-        public ObservableCollection<MessageInbox> MessageList
-        {
-            get
-            {
-                return _messageList;
-            }
+        public static Socket _socket;
+        
 
-            set
-            {                
-                _messageList = value;
-            }
-        }
-
-        public InboxIO(String hostURL, int portNumber)
-        {
-            _messageList = new ObservableCollection<MessageInbox>();
-            receivedMessage = new MessageInbox();
-            if (portNumber > 0)
-                _socket = IO.Socket(hostURL + ":" + portNumber);
-            else
-                _socket = IO.Socket(hostURL);
-
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Tick += updateMessageListUponReceivingMessage;
-            timer.Interval = new TimeSpan(0, 0, 1);
-            timer.Start();
-
-            _socket.On(Socket.EVENT_CONNECT, () =>
-            {
-                _socket.Emit("testwp", "hi");
-            });
-
-            _socket.On(Socket.EVENT_MESSAGE, (data) =>
-            {
-                JObject received = (JObject)data;
-                receivedMessage = received.ToObject<MessageInbox>();
-            });
-
-        }
-
-        private void updateMessageListUponReceivingMessage(object sender, object e)
-        {
-            if (this != null)
-            {
-                if (receivedMessage != null)
-                {
-                    _messageList.Add(receivedMessage);
-                    receivedMessage = null;
-                }
-            }
-        }
-                
-        public void registerClient()
-        {
-            if (MediateClass.UserVM.UserInfo != null)
-            {
-                _socket.Emit("storeMyID", MediateClass.UserVM.UserInfo.UserId);
-            }
-        }
-                
-        public void sendMessage(string message)
-        {
-            if (MediateClass.KiotVM != null && MediateClass.UserVM != null)
-            {
-                int receiverID = MediateClass.KiotVM.SelectedStore.OwnerId;
-                int userId = MediateClass.UserVM.UserInfo.UserId;
-                string name = MediateClass.UserVM.UserInfo.Avatar;
-                string avatar = MediateClass.UserVM.UserInfo.Avatar;
-                DateTime inboxDate = DateTime.Now;
-
-                MessageInbox inbox = new MessageInbox
-                {
-                    UserID = receiverID,
-                    OwnerID = userId,
-                    UserName = name,
-                    Avatar = avatar,
-                    InboxDate = inboxDate,
-                    Content = message
-                };
-
-                JObject jsonRequest = JObject.FromObject(inbox);
-                Debug.WriteLine("JSON: " + jsonRequest);
-                _socket.Send(jsonRequest);
-                _messageList.Add(inbox);
-            }
-        }
 
     }
 }

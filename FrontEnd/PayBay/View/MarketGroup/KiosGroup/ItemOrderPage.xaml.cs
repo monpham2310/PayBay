919edcc642c7b36785dc211ca65e73a8efeb60cc
Do Thanh Nam <dothanhnam285@gmail.com>
2016-04-04 18:56:09 +0700

    - Này thì push

--- FrontEnd/PayBay/View/MarketGroup/KiosGroup/ItemOrderPage.xaml.cs
+++ FrontEnd/PayBay/View/MarketGroup/KiosGroup/ItemOrderPage.xaml.cs
@@ -20,9 +20,9 @@ namespace PayBay.View.MarketGroup.KiosGroup
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class OrderPage : Page
+    public sealed partial class ItemOrderPage : Page
     {
-        public OrderPage()
+        public ItemOrderPage()
         {
             this.InitializeComponent();
         }
